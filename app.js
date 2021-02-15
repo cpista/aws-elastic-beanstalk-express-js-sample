c8000234c2dcaff188ee9bfb8bb85146d4201520
PISTA Ciprian Albert OSE <Ciprian.Pista@office.orange.intra>
2021-02-15 17:03:29 +0200

    Third test

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This will start learn me AWS!'));
-app.get('/', (req, res) => res.send('Second deployment of web-app using Pipe-line commmands'));
+app.get('/', (req, res) => res.send('Andy you can learn too Continuous integration in AWS with GIT & AWS stage'));
+
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
