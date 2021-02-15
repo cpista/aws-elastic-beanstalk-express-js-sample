a90c0bd5e0844e6568c41214f92c520dfaaea092
PISTA Ciprian Albert OSE <Ciprian.Pista@office.orange.intra>
2021-02-15 16:59:46 +0200

    Pipeline test

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('This will start learn me AWS!'));
+app.get('/', (req, res) => res.send('Second deployment of web-app using Pipe-line commmands'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
